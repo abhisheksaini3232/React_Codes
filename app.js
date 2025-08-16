dd4da0e9f67c07d123e463b40494947b32dac920
abhisheksaini3232 <abhishekbadthunia3232@gmail.com>
2025-08-16 22:01:40 +0530

    message

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 const root= createRoot(document.getElementById("root"));
-     const heading= React.createElement("h1",{},"Hey Bro You did it");
 
-     root.render(heading);
+const Header= () => {
+     return(
+          <h1>This is Header</h1>
+     )
+}
+
+     root.render(<Header/>);
